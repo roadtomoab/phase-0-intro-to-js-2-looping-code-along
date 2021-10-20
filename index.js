eef840d45b1ee3a2b8e105966e4ebdae6fb50f04
Noah Blumenstein <noah.blumenstein@gmail.com>
2021-10-19 22:34:32 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+let names = ["Lisa", "Kaitlin", "Jan"];
+const event = "surprise";
+let newArray = []
+
+function writeCards(names, event) {
+
+    for (let i = 0; i < 3; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return newArray;
+
+}
+
+let number = 10;
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
